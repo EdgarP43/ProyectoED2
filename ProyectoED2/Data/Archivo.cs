e45d04268deaaff2bfb2f66b5a17b94ffe060f29
EdgarP43 <e.paredes-cas@outlook.com>
2020-05-04 21:13:50 -0600

    Huffman Completo

---
+++ ProyectoED2/Data/Archivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoED2.Data
+{
+    public class Archivo
+    {
+        public string NombreArchivo { get; set; }
+        public double Razon { get; set; }
+        public double Factor { get; set; }
+        public double Porcentaje { get; set; }
+        public string NuevoNombre { get; set; }
+        public string RutaNuevoNombre { get; set; }
+
+        public Archivo()
+        {
+            NombreArchivo = "";
+            RutaNuevoNombre = "";
+            NuevoNombre = "";
+            Razon = 0;
+            Factor = 0;
+            Porcentaje = 0;
+        }
+    }
+}
