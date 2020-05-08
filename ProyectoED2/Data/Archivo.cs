0930acdc5610449938511e73fb434d606bf0f280
Cool232777 <cool232777@gmail.com>
2020-05-08 11:04:04 -0600

    Modelos de sucursales, producto y sucursal-precio agregadas

--- ProyectoED2/Data/Archivo.cs
+++ ProyectoED2/Data/Archivo.cs
@@ -2,26 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProyectoED2.Modelos;
+using System.IO;
 
 namespace ProyectoED2.Data
 {
     public class Archivo
     {
-        public string NombreArchivo { get; set; }
-        public double Razon { get; set; }
-        public double Factor { get; set; }
-        public double Porcentaje { get; set; }
-        public string NuevoNombre { get; set; }
-        public string RutaNuevoNombre { get; set; }
-
-        public Archivo()
-        {
-            NombreArchivo = "";
-            RutaNuevoNombre = "";
-            NuevoNombre = "";
-            Razon = 0;
-            Factor = 0;
-            Porcentaje = 0;
-        }
+        
     }
 }
--- ProyectoED2/Huffman/Huffman.cs
+++ ProyectoED2/Huffman/Huffman.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web;
+using ProyectoED2.Data;
 
 namespace ProyectoED2.Huffman
 {
@@ -25,7 +26,6 @@ namespace ProyectoED2.Huffman
         public Dictionary<string, Dictionary<byte, string>> CodigosPrefijo = new Dictionary<string, Dictionary<byte, string>>();
         public ArbolS Arbol = new ArbolS();
         public Dictionary<byte, string> CodigosPrefijoArchivoActual = new Dictionary<byte, string>();
-        public Dictionary<string, Archivo> DatosDeArchivos = new Dictionary<string, Archivo>();
         public Dictionary<string, byte> CodigoPD = new Dictionary<string, byte>();
         const int bufferLength = 1000;
         public int CompresiónHuffman(string path, string[] nombreArchivo, string pathHuffman, string NombreNuevo)
